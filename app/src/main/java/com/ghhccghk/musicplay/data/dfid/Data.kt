18cc215da48f332748b5cfccf1e69222e242557c
ghhccghk <2137610394@qq.com>
2026-02-05 20:45:48 +0800

    feat: 新增 DFID 数据模型
    为处理 DFID 相关的数据结构，增加了 `DfidData` 和 `Data` 两个数据类 (data class)。
    
    Signed-off-by: ghhccghk <2137610394@qq.com>

---
+++ app/src/main/java/com/ghhccghk/musicplay/data/dfid/Data.kt
@@ -0,0 +1,9 @@
+package com.ghhccghk.musicplay.data.dfid
+
+data class Data(
+    val dfid: String,
+    val guid: String,
+    val mac: String,
+    val mid: String,
+    val serverDev: String
+)
---
+++ app/src/main/java/com/ghhccghk/musicplay/data/dfid/DfidData.kt
@@ -0,0 +1,7 @@
+package com.ghhccghk.musicplay.data.dfid
+
+data class DfidData(
+    val `data`: com.ghhccghk.musicplay.data.Data,
+    val error_code: Int,
+    val status: Int
+)
